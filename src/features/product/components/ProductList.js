bb31a7803afb4bf64e93eef90e3327704da53bac
Umang Sorathiya <umang.sorathiya@krazybee.com>
2023-07-26 17:53:57 +0530

    Completed till Part 7 in office branch.

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -198,24 +198,40 @@ export default function ProductList() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const products = useSelector(selectAllProducts);
   const [filter, setFilter] = useState({});
+  const [sort, setSort] = useState({});
 
   const handleFilter = (e, section, option) => {
-    const newFilter = { ...filter, [section.id]: option.value };
+    console.log(e.target.checked)
+    const newFilter = { ...filter };
+    if(e.target.checked){
+      if(newFilter[section.id]){
+
+      
+      newFilter[section.id].push(option.value);
+      }  
+      else {
+        newFilter[section.id] =[option.value];
+      }
+    }
+    else {
+      const index = newFilter[section.id].findIndex(el=> el === option.value)
+      newFilter[section.id].splice(index,1);
+    }
+    console.log(newFilter);
     setFilter(newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
-    console.log(section.id, option.value);
+    
   };
 
   const handleSort = (e, option) => {
-    const newFilter = { ...filter, _sort: option.sort, _order:option.order };
-    setFilter(newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
+    const sort = { _sort: option.sort, _order:option.order };
+    setSort(sort);
+    
   };
 
 
   useEffect(()=> {
-    dispatch(fetchAllProductAsync());
-  },[dispatch])
+    dispatch(fetchProductsByFiltersAsync({filter, sort}));
+  },[dispatch,filter,sort])
 
   return (
     <div className="bg-white">
