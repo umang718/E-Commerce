30c7de87ac9e882e637cc614418ebe700e099314
umang718 <umangvirat@gmail.com>
2023-08-10 22:16:39 +0530

    Fixed Payment issue. Now working fine.

--- src/pages/StripeCheckout.js
+++ src/pages/StripeCheckout.js
@@ -11,7 +11,7 @@ import { selectCurrentOrder } from "../features/order/orderSlice";
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
 const stripePromise = loadStripe(
-  "pk_test_51N5NLVSF2Mo4AGVvYGMMNnTcPA5lHrkUv8zPymsKfL31c8m6et8525Y92zMgA22m1u6ZkkXsBofre0PJL6i3eVsh00sR6M8c00"
+  "pk_test_51Nd6FRSDlMlICsleg83nIK3Lqs3WgnMbBBVxlXJBlzacnwgyH2uw2TlrErFSZyb01ai2grmhI9MDLJIVsDHeeAKk00qAHFiBde"
 );
 
 export default function StripeCheckout() {
