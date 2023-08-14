4d29d685a2eb610f0b800e2669759e9d9dcf7f55
umang718 <umangvirat@gmail.com>
2023-08-14 21:31:25 +0530

    Changed the link in checkoutform.js for redirection.

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-nine-iota.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `https://mern-e-commerce-psi.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
