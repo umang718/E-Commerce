6ce74b668044a6d2245c68ac89d49240bec8b3ad
umang718 <umangvirat@gmail.com>
2023-08-14 23:37:14 +0530

    checkoutform.js link changed

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-e-commerce-psi.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `https://e-commerce-backend-green.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
